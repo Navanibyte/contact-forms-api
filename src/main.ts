b9c370f9756a9c0dd0749af050dc274365b2a8a6
navani <navani.nayak@impelox.com>
2025-11-24 10:24:55 +0530

    fixing the cors error

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -7,20 +9,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'https://formbuilder.impelox.com',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://formbuilder.impelox.com',
+        'https://www.formbuilder.impelox.com'
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Authorization'],
+    allowedHeaders: 'Content-Type,Authorization',
+    exposedHeaders: 'Authorization',
   });
 
   app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(process.env.PORT ?? 8000);
 }
-
 bootstrap();
