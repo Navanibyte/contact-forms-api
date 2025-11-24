15f003b2802f3a157b3ea194797b5682bdd58fb6
navani <navani.nayak@impelox.com>
2025-11-24 11:33:46 +0530

    fix corse error

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,52 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: (origin, callback) => {
+//       const allowedOrigins = [
+//         'http://localhost:3000',
+//         'http://localhost:5173',
+//         'http://127.0.0.1:3000',
+//         'https://formbuilder.impelox.com',
+//         'https://www.formbuilder.impelox.com'
+//       ];
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+//     credentials: true,
+//     allowedHeaders: 'Content-Type,Authorization',
+//     exposedHeaders: 'Authorization',
+//   });
+
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(process.env.PORT ?? 8000);
+// }
+// bootstrap();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'https://formbuilder.impelox.com',
-        'https://www.formbuilder.impelox.com'
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://127.0.0.1:3000',
+      'https://formbuilder.impelox.com',
+      'https://www.formbuilder.impelox.com'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
-    allowedHeaders: 'Content-Type,Authorization',
-    exposedHeaders: 'Authorization',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
 
   app.useGlobalPipes(new ValidationPipe());
