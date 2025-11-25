92adefaefc8aa4087ab8b7fb27217930452605ee
navani <navani.nayak@impelox.com>
2025-11-25 12:25:43 +0530

    fixing the auth

--- src/auth/services/oauth.strategies/google-oauth.strategy/google-oauth.strategy.service.ts
+++ src/auth/services/oauth.strategies/google-oauth.strategy/google-oauth.strategy.service.ts
@@ -14,7 +14,7 @@ export class GoogleOauthStrategyService extends PassportStrategy(Strategy, 'goog
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:3000/api/auth/callback/google',
+            callbackURL: process.env.GOOGLE_REDIRECT_URI,
             scope: ['email', 'profile'],
         });
     }
