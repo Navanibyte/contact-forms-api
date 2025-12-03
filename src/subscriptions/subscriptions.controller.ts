939e821f1edd063b6847200018095dbf9e264200
navani <navani.nayak@impelox.com>
2025-12-03 16:54:51 +0530

    feat: add authentication guard and endpoint to check subscription status

--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Request, Param, UseGuards } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Subscription } from '../entites/subscription.entity';
 import { Plan } from '../entites/plan.entity';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('subscriptions')
 export class SubscriptionsController {
@@ -33,4 +35,38 @@ export class SubscriptionsController {
       productinfo: `${plan.plan_name} Subscription`,
     };
   }
+
+
+@UseGuards(AuthGuard)
+@Get('is-subscribed')
+async isSubscribed( @Request() request: any) {
+  const user_id = request.user.userId;
+  console.log("user_id::::", user_id);
+  if (!user_id) {
+    return { error: 'user_id is required' };
+  }
+
+  // Get latest subscription of this user
+  const latest = await this.subRepo.findOne({
+    where: { user_id },
+    order: { id: 'DESC' },
+  });
+
+  if (!latest) {
+    return {
+      subscribed: false,
+      message: 'No subscription found',
+    };
+  }
+
+  // Check active
+  const isActive = latest.payment_status === 'ACTIVE';
+
+  return {
+    subscribed: isActive,
+    payment_status: latest.payment_status,
+    order_id: latest.order_id,
+    plan_id: latest.plan_id,
+  };
+}
 }
--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -5,9 +5,11 @@ import { Subscription } from '../entites/subscription.entity';
 import { Plan } from '../entites/plan.entity';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     TypeOrmModule.forFeature([Subscription, Plan])  // ✅ Fix here
   ],
   controllers: [SubscriptionsController],
